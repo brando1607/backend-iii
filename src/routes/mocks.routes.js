9c19cdb5b8752c5d3a1915a3c7cd9ae54bb5c568
brando1607 <109045330+brando1607@users.noreply.github.com>
2024-12-04 16:31:39 -0300

    http verb changes for mocks routes so it can be used through the url

--- src/routes/mocks.routes.js
+++ src/routes/mocks.routes.js
@@ -2,10 +2,10 @@ import { Router } from "express";
 import { MocksController } from "../controller/mocks.controller.js";
 export const mocksRouter = Router();
 
-mocksRouter.post("/users/:n", MocksController.createMockUsers);
+mocksRouter.get("/users/:n", MocksController.createMockUsers);
 
 mocksRouter.get("/users", MocksController.getUsers);
 
-mocksRouter.post("/products/:n", MocksController.createMockProducts);
+mocksRouter.get("/products/:n", MocksController.createMockProducts);
 
 mocksRouter.get("/products", MocksController.getProducts);
